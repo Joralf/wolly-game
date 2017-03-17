d03295abb3e9669e7b11f482327f2ef714bf4e37
Joralf Quist <j.quist@meteovista.nl>
2017-03-17 11:30:36 +0100

    two clouds with bg

--- src/states/load.js
+++ src/states/load.js
@@ -7,11 +7,15 @@ export class Load extends Phaser.State {
        let textStyle = {font: '45px Arial', alight: 'center', stroke: 'blue', fill: 'blue'};
 
        this.game.add.text(80, 150, 'loading...', textStyle);
-       this.game.load.spritesheet('wizard', '../assets/wizardsprite.png', 95, 123, 6);
-       this.game.load.spritesheet('player', '../assets/wol_logo.jpg', 51, 51);         
+
+
+       this.game.load.spritesheet('player', '../assets/player.jpg', 51, 51);
        this.game.load.image('background', '../assets/gamebg.jpg');
        this.game.load.spritesheet('player', '../assets/player.jpg', 51, 51);
-       this.game.load.spritesheet('cloud', '../assets/cloud-1.png', 283, 142);         
+       this.game.load.spritesheet('cloud', '../assets/cloud.png', 283, 142);
+       this.game.load.spritesheet('cloud-rain', '../assets/cloud-rain.png', 283, 500);
+
+
     }
 
     create() {
--- src/states/play.js
+++ src/states/play.js
@@ -3,26 +3,26 @@
 import Phaser from 'phaser';
 import { Player } from '../sprites/player';
 import { Cloud } from '../sprites/cloud';
+import { CloudRain } from '../sprites/cloud-rain';
 
 export class Play extends Phaser.State {
     create() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.background = this.game.add.tileSprite(0, 0, 800, 600, 'background');
 
         this.player = new Player(this.game, 20, this.game.height);
         //adding random clouds, feel free to refactor :)
-        this.cloud = new Cloud(this.game, 0, 0);
-        this.cloud1 = new Cloud(this.game, 500, 200);
-        this.cloud.scale.setTo(0.5, 0.5);
-        this.cloud1.scale.setTo(0.4, 0.4);
+        this.cloud = new Cloud(this.game, 600, 50);
+        this.cloudRain = new CloudRain(this.game, 400, 200);
+
         this.game.add.existing(this.player);
         this.game.add.existing(this.cloud);
-        this.game.add.existing(this.cloud1);
+        this.game.add.existing(this.cloudRain);
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.game.input.keyboard.addKeyCapture([ Phaser.KeyCode.SPACEBAR ]);
 
         //  The scrolling starfield background
-        this.background = this.game.add.tileSprite(0, 0, 800, 600, 'background');
     }
 
     update() {
