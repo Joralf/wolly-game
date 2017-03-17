18a858da8e905879639ef76602bbe639843e94fd
Vladimir Vujosevic <v.vujosevic@meteovista.nl>
2017-03-17 11:05:31 +0100

    add clouds

--- src/states/load.js
+++ src/states/load.js
@@ -8,7 +8,8 @@ export class Load extends Phaser.State {
 
        this.game.add.text(80, 150, 'loading...', textStyle);
        this.game.load.spritesheet('wizard', '../assets/wizardsprite.png', 95, 123, 6);
-       this.game.load.spritesheet('player', '../assets/player.jpg', 51, 51);         
+       this.game.load.spritesheet('player', '../assets/player.jpg', 51, 51);
+       this.game.load.spritesheet('cloud', '../assets/cloud-1.png', 283, 142);         
 
     }
 
